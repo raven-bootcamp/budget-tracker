26c9843dfc720a93474100b22c4e41fd12ea471d
Nick Lee <raven1300@hotmail.com>
2021-10-04 13:27:32 +1100

    updates for server js file including database variables

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+require("dotenv").config()
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,14 +17,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// database variables and booleans
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false, 
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}`);
 });
